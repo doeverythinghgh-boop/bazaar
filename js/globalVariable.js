e1be0e984c2b4f20ccb69044287aabf1f0182475
printcode1000-lgtm <print.code.1000@gmail.com>
2025-12-17 13:25:48 +0200

    767

--- js/globalVariable.js
+++ js/globalVariable.js
@@ -150,7 +150,7 @@ function productAddSetType(editMode = false) {
       );
     } else if (productTypeToAdd == 0) {
       mainLoader(
-        "./pages/productAdd.html",
+        "./pages/productAdd/productAdd.html",
         "index-product-container",
         0,
         undefined,
@@ -216,7 +216,7 @@ function loadProductForm(options = {}) {
   if (editMode) {
     pagePath = isService ? "./pages/productEdit2.html" : "./pages/productEdit.html";
   } else {
-    pagePath = isService ? "./pages/productAdd2.html" : "./pages/productAdd.html";
+    pagePath = isService ? "./pages/productAdd2.html" : "./pages/productAdd/productAdd.html";
   }
 
   console.log(`[ProductForm] تحميل صفحة ${editMode ? 'تعديل' : 'إضافة'} ${isService ? 'خدمة' : 'منتج'} للفئة ${categories.mainId}/${categories.subId}`);
